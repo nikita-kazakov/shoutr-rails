ea2a6357260002dd469a0e4e01030769506b1d6c
Your Name <contactnikitakazakov@gmail.com>
2019-10-06 15:23:24 -0600

    add root page and setup sign_in routes

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,4 +1,5 @@
 Clearance.configure do |config|
+  config.routes = false #Will no longer be using Clearance routes as we generated routes: rails g clearance:routes
   config.mailer_sender = "reply@example.com"
   config.rotate_csrf_on_sign_in = true
 end
